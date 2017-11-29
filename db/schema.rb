76f9f2bf60c867140f7a41ccb4ff2ede7805ea16
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-11-29 12:10:43 +0100

    added geocoding gem with basic config, and fixed user columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128144039) do
+ActiveRecord::Schema.define(version: 20171129104737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,10 +53,12 @@ ActiveRecord::Schema.define(version: 20171128144039) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "city"
+    t.string   "address"
     t.string   "accomodation"
     t.string   "description"
     t.string   "type"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
