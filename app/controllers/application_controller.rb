ab86e4a3b03d5442b0841c66db5cfee50b4b81e8
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-11-29 18:12:44 +0100

    fixed sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city, :address])
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
+  validates :city, presence: true
 
   mount_uploader :photo, PhotoUploader
 
