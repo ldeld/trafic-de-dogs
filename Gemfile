8bca5798c01cacea2d4a3e3eee548eada62515ba
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-11-29 12:23:22 +0100

    added markers to sitters index map

--- Gemfile
+++ Gemfile
@@ -17,6 +17,12 @@ gem 'jquery-rails'
 gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
+gem "coffee-rails"
+gem "gmaps4rails"
+
+source 'https://rails-assets.org' do
+  gem "rails-assets-underscore"
+end
 
 group :development, :test do
   gem 'pry-byebug'
--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -1,7 +1,12 @@
 class SittersController < ApplicationController
   before_action :find_sitter, only: [:show]
   def index
-    @sitters = Sitter.all
+    @sitters = Sitter.where.not(latitude: nil, longitude: nil)
+
+    @markers = Gmaps4rails.build_markers(@sitters) do |sitter, marker|
+      marker.lat sitter.latitude
+      marker.lng sitter.longitude
+    end
   end
 
   def show
