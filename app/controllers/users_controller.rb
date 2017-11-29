505f6d47873a530892b842b4c0d59c406355a161
Lena Rowe <lena@lenarowe.com>
2017-11-29 14:15:12 +0100

    second form ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # show, edit , update, destroy
 class UsersController < ApplicationController
+
   before_action :find_user, only: [:show]
+
   def index
     @users = User.all
   end
@@ -9,9 +11,23 @@ class UsersController < ApplicationController
     @dogs = @user.dogs.all
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update_attributes(user_params)
+    redirect_to user_path(@user)
+  end
+
   private
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:address, :accomodation, :description)
   end
 end
