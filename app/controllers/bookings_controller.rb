5a160a042e5a2bf5063b166c66930c4d514652c0
Lena Rowe <lena@lenarowe.com>
2017-11-29 11:59:23 +0100

    bookings page ok on big screen

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   # before_action :find_user, except: [:create]
 
   def index
-    @bookings = Booking.where(owner_id: current_user.id)
+    @bookings = Booking.where(owner_id: current_user.id).sort {|booking| booking.start_date}.reverse!
   end
 
   def new
@@ -10,7 +10,7 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(sitter_id: params[:sitter_id], owner_id: current_user.id, start_date: Date.parse("2017-12-30"), end_date: Date.parse("2017-12-31") )
+    @booking = Booking.new(sitter_id: params[:sitter_id], owner_id: current_user.id, start_date: Date.strptime("12/06/2018", "%d/%m/%Y"), end_date: Date.strptime("13/06/2018", "%d/%m/%Y"))
     if @booking.save
       redirect_to bookings_path
     else
