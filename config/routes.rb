fc08abdf0d5916b8e6b910432dcfd5346b2bdc86
Natacha Ducre <natacha.ducre@gmail.com>
2017-11-30 11:32:01 +0100

    first cards on home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get '/bookings', to: 'bookings#index'
   get '/edit-profile', to: 'users#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get 'sign-up', to: 'users#edit'
+  end
+
   resources :users do
     resources :dogs, only: [:index, :new, :create]
   end
