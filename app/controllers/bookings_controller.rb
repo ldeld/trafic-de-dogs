207489772a4f6ae345ab26a0cfd3ed78ec5e8d7f
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-11-30 17:07:57 +0100

    changed dog selecting to checkboxes (so you can book a walk for more than one dog)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,7 +13,9 @@ class BookingsController < ApplicationController
     # p params
     # binding.pry
     @booking = Booking.new(sitter_id: params[:sitter_id], owner_id: current_user.id, start_date: Date.strptime("12/06/2018", "%d/%m/%Y"), end_date: Date.strptime("13/06/2018", "%d/%m/%Y"))
-    BookDog.create!(booking: @booking, dog_id: params[:booking][:dog_ids])
+    params[:booking][:dog_ids].each do |dog_id|
+      BookDog.create!(booking: @booking, dog_id: dog_id) if dog_id.present?
+    end
     if @booking.save
       redirect_to bookings_path
     else
@@ -24,13 +26,5 @@ class BookingsController < ApplicationController
   def destroy
   end
 
-  private
 
-  def booking_params
-    params.require(:booking).permit(:sitter_id)
-  end
-
-  # def find_user
-  #   @user = User.find(params[:id])
-  # end
 end
